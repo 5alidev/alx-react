09424205c7a28a597f01efd1071fa5c230e2d260
5alidev <khalidbenhamou.dev@outlook.com>
2024-08-22 12:17:36 +0100

    Install Babel

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -30,6 +30,17 @@ module.exports = {
               'image-webpack-loader',
             ],
           },
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],   
+    
+              },
+            },
+          },
         ],
     },
 }
---
+++ 0x01-react_intro/task_5/dashboard/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import holberton_logo from './holberton_logo.jpg';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holberton_logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
