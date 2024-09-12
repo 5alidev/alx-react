15547bf8737cbed92068190bf9e3503ce89643f7
5alidev <khalidbenhamou.dev@outlook.com>
2024-09-13 00:05:43 +0100

    export func

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -3,22 +3,17 @@ import { Seq } from "immutable";
 const printBestStudents = (grades) => {
   const studentsSeq = Seq(grades);
 
-  const filtered = studentsSeq.filter((student) => {
-    student.firstName.charAt(0).toUpperCase();
-    return student.score > 70;
-  });
+  const bestStudents = studentsSeq.filter((student) => student.score >= 70);
 
-  function capFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-
-  const JSObject = filtered.toJS();
-
-  Object.keys(JSObject).map((key) => {
-    JSObject[key].firstName = capFirstLetter(JSObject[key].firstName);
-    JSObject[key].lastName = capFirstLetter(JSObject[key].lastName);
-    return JSObject[key];
-  });
+  const capitalizedStudents = bestStudents.map((student) => ({
+    ...student,
+    firstName:
+      student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
+    lastName:
+      student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
+  }));
 
   console.log(capitalizedStudents.toJS());
 };
+
+export default printBestStudents;
